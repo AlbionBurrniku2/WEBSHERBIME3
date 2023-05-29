ad9f6d44ed36962ac4788632555e6842fc52ef65
AlbionBurrniku <74713774+AlbionBurrniku2@users.noreply.github.com>
2023-05-29 22:10:51 +0200

    Add files via upload

---
+++ Data/CSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CSBLOG.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSBLOG.Data
+{
+    public class CSDbContext: DbContext
+    {
+        public CSDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Autoret> Autorets { get; set; }
+        public DbSet<Postet> Postets { get; set; }
+        public DbSet<Foto> Fotos { get; set; }
+
+    }
+}
